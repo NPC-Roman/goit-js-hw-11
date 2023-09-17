d733622fe49e75db28219a99a81fde05b3c61ae7
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-09-17 21:22:02 +0000

    Deploying to gh-pages from @ NPC-Roman/goit-js-hw-11@cdd1b1398457c015d16488eeb0a2bb5e6ea26c5b 🚀

--- index.77c5ebe2.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:800,pagination:{el:".swiper-pagination",allowSlideNext:!0,clickable:!0,dynamicBullets:!0}});
-//# sourceMappingURL=index.77c5ebe2.js.map
